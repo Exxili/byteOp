c844db7d99665368a7c3c7a55e479c7b4f0880ac
Exxili <sirvaux@live.com>
2021-10-27 08:46:02 -0700

    fixed some eslint issues and updated jest config

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { toFloat32, fromFloat32 } from './float32'
-import { toInt32, fromInt32 } from './int32'
-import { toInt16, fromInt16 } from './int16'
-import { toUint32, fromUint32 } from './uint32'
-import { fromUint8 } from './uint8'
+import { toFloat32, fromFloat32 } from "./float32";
+import { toInt32, fromInt32 } from "./int32";
+import { toInt16, fromInt16 } from "./int16";
+import { toUint32, fromUint32 } from "./uint32";
+import { fromUint8 } from "./uint8";
 
 export {
-    toFloat32,
-    fromFloat32,
-    toInt32,
-    fromInt32,
-    toInt16,
-    fromInt16,
-    fromUint8,
-    toUint32,
-    fromUint32,
-}
+  toFloat32,
+  fromFloat32,
+  toInt32,
+  fromInt32,
+  toInt16,
+  fromInt16,
+  fromUint8,
+  toUint32,
+  fromUint32,
+};
--- src/int16.ts
+++ src/int16.ts
@@ -1,6 +1,6 @@
 /**
- * Returns a Int16 Number from an Array of 4 Uint8's
- * @param {number} bytes - The Number as Uint8Array of 4 Bytes
+ * Returns a Int16 Number from an Array of 2 Uint8's
+ * @param {number} bytes - The Number as Uint8Array of 2 Bytes
  * @param {number} decimalPlaces - How many decimal places
  * @returns {number|Error} returns Int16 Number else returns Failure Error if
  * byte array is incorrect length
@@ -20,7 +20,7 @@ export function toInt16(bytes: Uint8Array): number | Error {
 /**
  * Returns an array of Uint8 Values from a Int16 number
  * @param {number} int - The Number as a Int16, range must be within -32767 - 32768
- * @returns {number} returns Uint8Array[4]
+ * @returns {number} returns Uint8Array of length 2
  * @example
  * const number = 14640
  * const arr = fromInt16(number)
@@ -29,7 +29,7 @@ export function toInt16(bytes: Uint8Array): number | Error {
  */
 export function fromInt16(int: number): Uint8Array {
   if (int > 32767 || int < -32768) {
-    throw Error("Outside Range Error, int16 must be between -32768 and 32767");
+    throw Error("Outside Range Error, Int16 must be between -32768 and 32767");
   }
   const int16Array = new Int16Array([int]);
   return new Uint8Array(int16Array.buffer);
